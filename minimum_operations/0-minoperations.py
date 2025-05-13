034b7e3fa6a8cf4feb41f89e451dab34c7411d5f
Lightstats <hirwa12ariane@gmail.com>
2025-05-13 17:47:37 +0200

    initial

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Minimum operations to get n characters"""
+
+
+def factorize(num):
+    div = 2
+    factors = []
+    while num > 1:
+        if num % div == 0:
+            factors.append(div)
+            num /= div
+        else:
+            div += 1
+    return factors
+
+
+def minOperations(n):
+    factors = factorize(n)
+    clust = {}
+    for factor in factors:
+        clust[factor] = factors.count(factor)
+    num_of_operations = 0
+    for key, value in clust.items():
+        num_of_operations += key*value
+    return num_of_operations
+
+
+print(minOperations(4))
